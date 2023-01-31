85fc6b56db74a1b38d3d36bb758106ca294925a6
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-01-31 23:27:27 +0530

    Add files via upload

---
+++ day1/level3/solution3.js
@@ -0,0 +1,7 @@
+let sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+let a=sentence.replace('%',' ')
+let b=sentence.replace('$',' ')
+let c=sentence.replace('@','')
+let d=sentence.replace('%','')
+let e=sentence.replace('#','')
+console.log(e)
