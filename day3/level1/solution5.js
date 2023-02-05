441b65cc91eda552eabd1ded0712701c2bcb92a3
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-02-05 23:59:21 +0530

    Add files via upload

---
+++ day3/level1/solution5.js
@@ -0,0 +1,19 @@
+console.log(4>3)
+console.log(4>=3)
+console.log(4<3)
+console.log(4==4)
+console.log(4===4)
+console.log(4!=4)
+console.log(4!==4)
+console.log(4!='4')
+console.log(4=='4')
+console.log(4==='4')
+console.log(4<=3)
+let a='python'
+let b='jargon'
+if(a.length!=b.length){
+    console.log(false)
+}
+else{
+    console.log(true)
+}
