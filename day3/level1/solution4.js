d0390fe506c1e06ee64b24315d369b722c9d339c
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-02-05 23:59:11 +0530

    Add files via upload

---
+++ day3/level1/solution4.js
@@ -0,0 +1,15 @@
+console.log(5)
+console.log('Monisha')
+let a=5
+let b=5
+if(a===b){
+    console.log(true)
+}
+
+//falsy
+let c=77
+let d=77
+console.log(c-d)
+console.log("")
+let firstName
+console.log(firstName)
