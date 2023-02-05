52abe6a4ef59e3faf0728e3a841b235db5cd3336
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-02-05 23:58:40 +0530

    Add files via upload

---
+++ day3/level1/solution1.js
@@ -0,0 +1,15 @@
+let firstName='Monisha'
+let lastName='Nagpal'
+let country='India'
+let city='Jaipur'
+let age=20
+let isMarried='yes'
+let year=2022
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(city))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(year))
+
