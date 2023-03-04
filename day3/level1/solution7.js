441c074414aa130d3423aceb63ba184cf50b88f5
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-03-04 22:59:38 +0530

    Add files via upload

---
+++ day3/level1/solution7.js
@@ -0,0 +1,8 @@
+const now=new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth())
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getTime())
