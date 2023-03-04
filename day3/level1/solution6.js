b1072dfb9f0eb8371a24143fe50b6848c41b304c
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-03-04 22:59:21 +0530

    Add files via upload

---
+++ day3/level1/solution6.js
@@ -0,0 +1,18 @@
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(!(4 > 3))
+console.log(!(4 < 3))
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12))
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+let a='python'.includes('on')
+let b='jargon'.includes('on')
+if(a===b){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
