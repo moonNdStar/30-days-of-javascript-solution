6322201a6d6e1b0d9ab6f183e5d4c46d72597472
moonNdStar <77716940+moonNdStar@users.noreply.github.com>
2023-03-04 23:00:57 +0530

    Add files via upload

---
+++ day3/level2/solution2.js
@@ -0,0 +1,9 @@
+let a=prompt('Enter side a')
+console.log(a)
+let b=prompt('enter side b')
+console.log(b)
+let c=prompt('enter side c')
+console.log(c)
+
+let per=parseInt(a)+parseInt(b)+parseInt(c)
+console.log(`The perimeter of the triangle is ${per}`)
